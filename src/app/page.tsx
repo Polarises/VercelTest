45ee216291d325baef027b78f8e3b05f8d5c261e
DT <polarises@live.com>
2023-02-21 18:05:53 +0800

    完成了时间

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image'
+
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+// const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
-    <main className={styles.main}>
-        <h2 >你好 任晗晗~</h2>
-    </main>
+    <div >
+
+    </div>
   )
 }
---
+++ src/pages/me.tsx
@@ -0,0 +1,35 @@
+import React, {useEffect, useMemo, useState} from 'react';
+import styles from './styles.module.scss';
+import dayjs from "dayjs";
+import NumberDisplay from "@/components/NumberDisplay";
+import localFont from "@next/font/local";
+// 1. 做一个垂直的 0-9 div
+// 2. 滚动到 7 时，div 就要垂直移动到 7 * height 个单位
+function App() {
+    const [time, setTime] = useState<number>(Date.now().valueOf());
+    useEffect(() => {
+        setInterval(() => {
+            setTime(Date.now().valueOf());
+        }, 1000);
+    }, []);
+
+    const timeStr = useMemo(() => {
+        let todayDate = dayjs(time).format('YYYY-MM-DD HH:mm:ss');
+        const diff =  dayjs(todayDate).diff('2020-11-28','seconds');
+        const day = Math.floor(diff / 86400); //天
+        const hours = Math.floor((diff % 86400)/3600); //时
+        const minute = Math.floor(((diff % 86400)%3600) / 60) //分
+        const second = ((diff % 86400)%3600) % 60 //秒
+        // console.log( `${day}：${hours} ：${minute} ：${second}`)
+        return `${day}:${hours}:${minute}:${second}`
+        // return dayjs(todayDate).diff('2020-11-28','seconds').toString();
+    }, [time]);
+
+    return (
+        <div className={styles.app}>
+            <NumberDisplay numberString={timeStr} />
+        </div>
+    );
+}
+
+export default App;
