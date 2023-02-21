2938f6edb0ef6ece7adafee9a12bc98391b4a2cc
DT <polarises@live.com>
2023-02-21 18:22:38 +0800

    add h2

--- src/components/NumberDisplay/index.tsx
+++ src/components/NumberDisplay/index.tsx
@@ -12,28 +12,32 @@ const NumberDisplay: FC<Props> = (props) => {
     const { numberString } = props;
 
     return (
-        <div className={styles.numberDisplay}>
-            {/* ['1', '2', '3']*/}
-            {numberString.split('').map((digitStr, index) => (
-                <div className={styles.digitWrapper} key={index}>
-                    {
-                        digitRegexp.test(digitStr) ? (
-                            // 展示数字
-                            <span className={styles.digitList} style={{
-                                transform: `translate(-50%, ${-Number(digitStr) * 32}px)`
-                            }}>
+      <div>
+          <h2>有奖问答，这是什么时间。</h2>
+          <div className={styles.numberDisplay}>
+              {/* ['1', '2', '3']*/}
+
+              {numberString.split('').map((digitStr, index) => (
+                  <div className={styles.digitWrapper} key={index}>
+                      {
+                          digitRegexp.test(digitStr) ? (
+                              // 展示数字
+                              <span className={styles.digitList} style={{
+                                  transform: `translate(-50%, ${-Number(digitStr) * 32}px)`
+                              }}>
                 {digits.map(digit => (
                     <span className={styles.digit} key={digit}>{digit}</span>
                 ))}
               </span>
-                        ) : (
-                            // 展示普通字符
-                            <span>{digitStr}</span>
-                        )
-                    }
-                </div>
-            ))}
-        </div>
+                          ) : (
+                              // 展示普通字符
+                              <span>{digitStr}</span>
+                          )
+                      }
+                  </div>
+              ))}
+          </div>
+      </div>
     )
 }
 
