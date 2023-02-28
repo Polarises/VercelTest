1e71264c348403791fdb1c7a8e1508727d0a3c73
DT <polarises@live.com>
2023-02-28 14:17:11 +0800

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-
-import { Inter } from '@next/font/google'
+import './global.css';
+// import {Inter} from '@next/font/google';
 // const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
-  return (
-    <div >
-
-    </div>
-  )
+    return (
+        <div className='wrap'>
+            <h2> Hi! </h2>
+        </div>
+    )
 }
--- src/pages/me/components/NumberDisplay/index.tsx
+++ src/pages/me/components/NumberDisplay/index.tsx
@@ -1,4 +1,4 @@
-import {FC, useState} from "react";
+import {FC} from "react";
 import styles from './styles.module.scss';
 
 const digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
